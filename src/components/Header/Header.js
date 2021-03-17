a515df449867595ad9621acbe7c85abd4663f034
aucoeur <aucoeur.ngo@students.makeschool.com>
2021-03-17 02:22:04 -0700

    added resume, tweaked colors.. wip

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Header.css';
 
+import Icon from '../Icon/Icon';
+import pdf_icon from '../../assets/pdf_icon.svg';
+
 function Header() {
   return (
     <div className="Header">
@@ -9,36 +12,31 @@ function Header() {
         <h3>Professional Dilettante.  Aspiring Software Engineer.</h3>
       </div>
 
-      <div className="animation">
-         <div className="threedp">
-            <div className="box">
-                <div className="inner">
-                    <svg
-                        viewBox="-55.5 -40 150 130"
-                        fill="transparent" stroke="orangered"
-                        strokeWidth="4"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path d="M 20 0 V 8 H 32 Q 36 12 32 16 H 4 Q 0 20 4 24 H 64 Q 68 28 64 32 H -28 Q -32 36 -28 40 H 64 Q 68 44 64 48 H -28 Q -32 52 -28 56 H 64 Q 68 60 64 64 H -30" />
-                    </svg>
-                    <div className="bars"></div>
-                    <div className="hotend"></div>
-                </div>
-                <div className="bed"></div>
+      <Icon />
+
+      <div className="badges">
+        <div className="col">
+          <div className="row">
+            <span className="res">
+            <a href={process.env.PUBLIC_URL + 'AucoeurNgo-Resume.pdf'}>
+              <img src="https://img.shields.io/badge/resume-darkred.svg?style=for-the-badge&labelColor=darkred" alt="pdf badge text"/>
+              <img src={pdf_icon} className="res-icon" style={{ width: 28 }}
+              alt="pdf icon" /></a></span>
+
+            <a href="https://github.com/aucoeur"><img src="https://img.shields.io/badge/aucoeur-grey?style=for-the-badge&logo=github&logoColor=white&labelColor=24292e" alt="github badge"/></a>
             </div>
-            <div className="spool"></div>
+          <div className="row">
+            <a href="mailto:aucoeurngo@gmail.com"><img src="https://img.shields.io/badge/contact_me-red?style=for-the-badge&logo=gmail&logoColor=white&labelColor=D14836" alt="email badge" /></a>
           </div>
         </div>
 
-      <div className="badges">
           <div className="col">
+             <div className="row">
               <a href="https://www.linkedin.com/in/aucoeurngo/"><img src="https://img.shields.io/badge/AUCOEUR_NGO-blue?style=for-the-badge&logo=linkedin&labelColor=005b8e" alt="linkedin badge" /></a>
-
+            <div className="row">
               <a href="https://medium.com/@aucoeurngo"><img src="https://img.shields.io/badge/@aucoeurngo-333333?style=for-the-badge&logo=medium&logoColor=white&labelColor=black" alt="medium badge" /></a>
-          </div>
-          <div className="col">
-            <a href="https://github.com/aucoeur"><img src="https://img.shields.io/badge/aucoeur-grey?style=for-the-badge&logo=github&logoColor=white&labelColor=24292e" alt="github badge"/></a>
-
-            <a href="mailto:aucoeurngo@gmail.com"><img src="https://img.shields.io/badge/contact_me-red?style=for-the-badge&logo=gmail&logoColor=white&labelColor=D14836" alt="email badge" /></a>
+            </div>
+            </div>
           </div>
         </div>
     </div>
