210402a3c83b460a97d9e6fcc53ce4992fed72de
aucoeur <aucoeur.ngo@students.makeschool.com>
2021-03-18 01:13:23 -0700

    going to need to redo this animtion from the gground up or use a library

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -8,8 +8,6 @@ function Icon() {
                 <div className="inner">
                     <svg
                         viewBox="-55.5 -40 150 130"
-                        fill="transparent" stroke="orangered"
-                        strokeWidth="4"
                         xmlns="http://www.w3.org/2000/svg">
                         <path d="M 20 0 V 8 H 32 Q 36 12 32 16 H 4 Q 0 20 4 24 H 64 Q 68 28 64 32 H -28 Q -32 36 -28 40 H 64 Q 68 44 64 48 H -28 Q -32 52 -28 56 H 64 Q 68 60 64 64 H -30" />
                     </svg>
