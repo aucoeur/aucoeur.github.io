69d61db967c0683398d9754c391366ba33b2ba96
aucoeur <stine@aucoeur.dev>
2025-09-14 17:52:11 -0700

    Migrate from gh-pages to Github Actions for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { copyFileSync } from 'fs'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-cname',
+      closeBundle() {
+        copyFileSync('CNAME', resolve('dist', 'CNAME'))
+      }
+    }
+  ],
   base: '/',
   build: {
     outDir: 'dist',
