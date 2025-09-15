e78e2c6399fc87fdd34363860de34c59abb3069e
aucoeur <stine@aucoeur.dev>
2025-09-14 17:46:09 -0700

    updated index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import './assets/font.css'
-import './assets/theme.css';
+import "./assets/theme.css";
 
-import App from './App';
+import App from "./App";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
